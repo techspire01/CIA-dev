76c6393fa2922b00dce260fe56f9d9abce9645ec
techspire01 <mohanbabuscsda2023@sankara.ac.in>
2025-08-30 01:37:15 +0530

    'altered'

--- proj/settings.py
+++ proj/settings.py
@@ -90,6 +90,8 @@ WSGI_APPLICATION = 'proj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+import os
 
 DATABASES = {
     'default': dj_database_url.config(
